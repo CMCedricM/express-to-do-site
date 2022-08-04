f230d4625c72e7f615a8a4f7543c1fc0ab31724a
CMCedricM <52368680+CMCedricM@users.noreply.github.com>
2022-08-03 22:49:03 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,300 @@
+// Standard Libraries
+const https = require('https'); 
+const express = require('express'); 
+const session = require('express-session'); 
+const cors = require('cors'); 
+const htmlRender = require('express-es6-template-engine');
+const path = require('path'); 
+
+// Additional Libraries
+const bcrypt = require('bcrypt');
+const fs = require('fs');
+const uuid = require('uuid')
+
+const {MongoClient} = require('mongodb');
+
+const dateTime = require('node-datetime');
+const { createServer } = require('http');
+const req = require('express/lib/request');
+
+
+// Environment 
+const TWO_HRS = 1000 * 60 * 60; 
+
+const {
+    SESS_LIFETIME = TWO_HRS, 
+    SESS_SECRET = "place-a-secret-key-here-lool",
+    NODE_ENV = 'development', 
+    SESS_NAME = 'sid',
+
+    //CERTS
+    // certFile = fs.readFileSync(path.join(__dirname, 'certs','certificate.pem')),
+    // keyFile = fs.readFileSync(path.join(__dirname, 'certs','key.pem')),
+    /*httpsOptions = {
+        cert: certFile,
+        key: keyFile
+    }*/
+} = process.env 
+
+
+
+// HTML File Paths 
+const staticFolder = path.join(__dirname, 'static'); // /Users/cedric-men/{dir}/node-project/static
+const dynamicPagesFolder = path.join(__dirname, 'views'); // /Users/cedric-men/{dir}/node-project/views
+
+
+// Database Connection Info 
+let db = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8')); // /Users/cedric-men/{dir}/node-project/db.json 
+const dbURL = `mongodb://${db.username}:${db.password}@${db.url}`;
+// db sources
+const userDbName = "home-users"; 
+const userDbCollection = "users";
+const toDoListCollection = "toDoData"
+
+class mySite{
+    constructor(){
+        // Start Variables
+        this.PORT = 5150; 
+        this.server = express(); 
+
+        // Sets Uses 
+        this.server.use(express.static(staticFolder)); 
+        this.server.use(express.json()); 
+        this.server.use(cors()); 
+
+        // Set Rendering Engines -> Middleware
+        this.server.engine('html', htmlRender); 
+        this.server.set('views', dynamicPagesFolder); 
+        this.server.set('view engine', 'html'); 
+
+        // Bind Function to Class
+        //this.runtime = this.runtime.bind(this); <--- Example of binding
+        this.login = this.login.bind(this);
+        this.signup = this.signup.bind(this);
+        this.logEvent = this.logEvent.bind(this);
+        this.addDataTodo = this.addDataTodo.bind(this);
+        this.getToDoList = this.getToDoList.bind(this);
+
+        // Check for auth 
+        this.authenticated = false; 
+
+        this.server.use(session({
+            name : SESS_NAME, 
+            resave: false, 
+            saveUninitialized: false, 
+            secret: SESS_SECRET, 
+            cookie : {
+                maxAge: SESS_LIFETIME, 
+                sameSite: true, // or strict
+                secure: NODE_ENV === 'production', 
+            }
+        })
+        )
+    }
+    
+    getTime(){
+        let dt = dateTime.create(); 
+        let logDateTime = dt.format('Y-m-d H:M:S')
+        return logDateTime;
+    }
+
+    logEvent(text){
+        console.log(`${this.getTime()} ----> ${text}`)
+    }
+
+    async login(req, res){
+
+        // Get User Info
+        if(!req){this.logEvent("Login Function Failure!"); try{res.send("Internal Server Error, please try again later...")}catch(err){this.logEvent(`${err}`)}; return;}
+        const {email, password} = req.body; 
+        console.log(email)
+        const client = new MongoClient(dbURL, {useNewUrlParser: true}); 
+        try{
+            await client.connect(); 
+            const db = client.db(userDbName);
+            const users = db.collection(userDbCollection); 
+            const user = await users.findOne({email : email.toLowerCase()}); 
+         
+
+            if(user){
+                const valid = await bcrypt.compare(password, user.password); 
+                if(valid){
+                    req.session.userId = user.firstname;
+                    this.logEvent("SUCCESSFUL login Attempt!");
+                    //console.log(`${logDateTime} ----> SUCCESSFUL login Attempt!`)
+                    res.send('/dashboard'); 
+                }
+                else{
+                    this.logEvent("FAILED login Attempt! => Invalid Credentials");
+                    //console.log(`${logDateTime} ----> FAILED login Attempt! => Invalid Credentials`)
+                    res.status(401).send('Email or Password Incorrect'); 
+                }
+            }
+            else{
+                this.logEvent("FAILED login Attempt => No Account");
+                //console.log(`${logDateTime} ----> FAILED login Attempt => No Account`)
+                res.status(401).send('No Account Found, Please Create One First');
+            }
+        }
+        catch(err){
+            this.logEvent(`System Error => ${err}\n\n`);
+            //console.log(`\n${logDateTime} ----> System Error => ${err}\n\n`);
+        }
+        finally{
+            client.close(); 
+        }
+    }
+
+
+    async createUser(req){
+        let user = {
+            firstname: req.body.firstname, 
+            lastname : req.body.lastname, 
+            email: (req.body.email).toLowerCase(),
+            password: await bcrypt.hash(req.body.password, 10)
+        }
+        return user;
+    }
+
+
+    async signup(req, res){
+        if(!req){this.logEvent("Signup Function Failure!"); try{res.send("Internal Server Error, please try again later...")}catch(err){this.logEvent(`${err}`)}; return;}
+        // Just Cause, set the req.session.userid to non 
+        req.session.destroy();
+        const user = await this.createUser(req); 
+        const client = new MongoClient(dbURL, {useNewUrlParser: true}); 
+        try{
+            await client.connect(); 
+            const db = client.db(userDbName);
+            const users = db.collection(userDbCollection); 
+            const checkAUser = await users.findOne({email : (req.body.email).toLowerCase()});
+            // Check if user already exists
+            if(checkAUser){ this.logEvent("Duplicate User Creation Attempted!"); res.status(401).send('User Already Exists!'); }
+            else{
+                await users.insertOne(user); 
+                this.logEvent("User Created");
+                //console.log('User Created');
+                res.send('/login'); 
+            }
+        }
+        catch(err){
+            this.logEvent(`${err}`);
+        }
+        finally{
+            client.close(); 
+        }
+    }
+
+    
+
+    // This area will handle to do list actions
+
+    async getToDoList(req, res){
+        if(!req.session.userId){ res.redirect('/login'); }
+        const user = req.session.userId; 
+        let data = {}; 
+
+        // Connect the Database
+        const client = new MongoClient(dbURL, {useNewUrlParser: true}); 
+        try{
+            await client.connect(); 
+            const db = client.db(userDbName); 
+            const userData = db.collection(toDoListCollection); 
+            const userRecords = await userData.findOne({'userID' : (user.toLowerCase())}); 
+            // Check if the user has any data attached to them 
+            if(!userRecords){
+                this.logEvent("Data Retrieved")
+                await userData.insertOne({'userID' : (user.toLowerCase()), 'toDoListData' : [] }); 
+                data = {
+                    userID: user
+                }
+            }else{
+                data = userRecords
+            }
+        
+        }catch(err){
+            this.logEvent(`Unable to Retrieve Data to This User! => ${err}`)
+            res.status(500).send("Unable to Add Item"); 
+        }finally{
+            client.close()
+        }
+
+        res.status(200).json(data);
+
+    }
+
+
+  
+    async addDataTodo(req, res){
+        if(!req.session.userId) res.redirect('/login'); 
+        const user = req.session.userId; 
+        // Lets Get the New Items Here
+        const {newItems} = req.body
+        if(newItems.itemID == null){newItems.itemID = uuid.v4(); }
+        console.log(newItems.itemID)
+        // Connect the Database
+        const client = new MongoClient(dbURL, {useNewUrlParser: true}); 
+        try{
+            await client.connect(); 
+            const db = client.db(userDbName); 
+            const userData = db.collection(toDoListCollection); 
+            const data = await userData.findOne({'userID' : (user.toLowerCase())}); 
+            // Check if the user has any data attached to them 
+            if(!data){
+                this.logEvent("Data Inserted")
+                await userData.insertOne({'userID' : (user.toLowerCase()), 'toDoListData' : [] }); 
+            }
+            userData.updateOne({'userID' : (user.toLowerCase())}, {$push: {'toDoListData' : newItems }}, (err, res) => {
+                if(err) this.logEvent(`There was an error appending the data ==> ${err}`); 
+                client.close()
+            })
+        
+        }catch(err){
+            this.logEvent(`Unable to Add Data to This User! => ${err}`)
+            res.send("Unable to Add Item"); 
+        }
+        this.logEvent("Item Was Added Successfully to this user!"); 
+        res.send(newItems.itemID); 
+        
+
+    }
+
+    // This will take the recieved id and compare it with all uuids in the toDoData and update the status of
+    // whether it was complete or not. 
+    // Problem: The server and the client side will have different ids if an item was added but the page was not reloaded
+    updateStatus(req,res){
+        return; 
+    }
+    
+
+    runtime = () => {
+        
+        // Home Routes
+        this.server.get('/home', (req,res) => { res.redirect('/') } )
+        // Login Routes
+        this.server.get(`/login`, (req, res) => { req.session.userId ? res.redirect('dashboard') : res.render('login')});
+        this.server.post('/login', this.login); 
+        // Signup Routes
+        this.server.get('/signup', (req, res) => {res.render('signup'); });
+        this.server.post('/signup', this.signup);
+        // After Login Routes
+        this.server.get('/dashboard', (req,res) => { req.session.userId ? res.render('toDoPage') : res.redirect('/login'); });
+        this.server.get('/userData', this.getToDoList); 
+        this.server.post('/userData', this.addDataTodo)
+        
+        // Logout Routes
+        this.server.get('/logout', (req,res) => { req.session.destroy(); res.redirect('/login'); });
+        
+        // Catch Bad URL
+        this.server.all('*', (req, res) => { res.status(404).render('badPage'); } )
+        
+        // For HTTPS 
+        //https.createServer(httpsOptions, this.server).listen(this.PORT, () => { console.log(`Server Listening at Port ${this.PORT}\n\n`); } )
+        // Without HTTPS
+        this.server.listen(this.PORT, () => { console.log(`Server Listening at Port ${this.PORT}\n\n`); });
+    }
+}
+
+
+app = new mySite(); 
+app.runtime();
---
+++ static/scripts/script.js
@@ -0,0 +1,126 @@
+const xhr = new XMLHttpRequest()
+
+document.getElementById('add-data').addEventListener('click', addItemsToList)
+
+
+function addEventListeners(){
+    const elements = document.getElementsByClassName('check-button') 
+    for(let i = 0; i < elements.length; i++){
+        elements[i].addEventListener('click', handleCheckBoxes, false);
+    }
+}
+
+function handleCheckBoxes(){
+    // Need to Convert to JSON with appropriate fields
+    let data = {
+        newItems : {
+             itemID : this.id, 
+             Name : this.value, 
+             Status : this.checked
+        }
+    };
+    console.log(data);
+
+}
+
+
+function updateDB(itemName, status, id, tempId = null){
+    // Need to Convert to JSON with appropriate fields
+    let itemidenti = '';
+    if(tempId != null){ itemidenti = null}
+    else{ itemidenti = id; }
+    let data = {
+        newItems : {
+             itemID : itemidenti, 
+             Name : `${itemName}`, 
+             Status : status
+        }
+    };
+
+    xhr.open('POST', '/userData', true); 
+    xhr.onload = () => { 
+        if(xhr.status != 200){ console.log("Could not add item to Cloud!"); }
+        else if(tempId != null) { document.getElementById(id).id= xhr.responseText; }
+    }
+    xhr.onerror = () => { console.log(`Fatal Error: ${xhr.response}`)}
+    xhr.setRequestHeader('Content-Type', 'application/json'); 
+    xhr.send(JSON.stringify(data)); 
+
+}
+
+
+function createAListItem(parent, itemName, checked, id=null){
+     // Create my Elements
+     let input = document.createElement('input'); 
+     let label = document.createElement('label');
+     let br = document.createElement('br');
+     let span = document.createElement('div')
+
+     // Create a Temporary ID => We will need this later if we complete the item without reloading the page
+     let tempId = null; 
+     // Maybe I should request a uuid here from server, so that its easy to update the db about a completed item later?
+     if(id == null){ tempId = parent.children.length + 1}
+     else{tempId = id; }  
+     // Set their attributes
+     input.setAttribute('type', 'checkbox' ); 
+     if(checked){ input.setAttribute('checked', 'checked'); }
+     input.setAttribute('id', `${tempId}`);
+     label.setAttribute('for', `${tempId}`);
+     input.value=`${itemName}`;
+     input.className = 'check-button';
+     label.className = 'list-element';
+     label.innerText = `${itemName}`;
+     span.className = 'list-element-span'
+     // Append Created Elements
+     span.append(input)
+     span.append(label)
+     span.append(br)
+     parent.append(span);
+     
+
+     return tempId;
+     
+}
+
+
+function addItemsToList(){
+    const inputField = document.getElementById('add-item-field'); 
+    // Check if the input field is an empty String
+    if(!(inputField.value).trim().length){ return; }
+    // Now Create the Element and add to the list
+    tempID = createAListItem(document.querySelector('#list'), inputField.value, false);
+    //Send the data to the db 
+    updateDB(inputField.value, 0, tempID, tempID)
+
+    inputField.value = '';
+     
+}
+
+
+function loadData(data){
+    if(!data){ return; }
+    const list = document.querySelector('#list'); 
+    data.forEach((element) => {
+        console.log(`Appending ${element.Name}`); 
+        createAListItem(list, `${element.Name}`, element.status, element.itemID)
+    });
+    addEventListeners()
+}
+
+
+function showList(){
+    xhr.open("GET", "/userData", true); 
+    xhr.onload = () =>{
+        if(xhr.status != 200){
+            console.log("Errror" , xhr.responseText); 
+        }else{
+            document.getElementById('welcome-user').textContent = `Welcome, ${JSON.parse(xhr.responseText).userID}!`
+            loadData(JSON.parse(xhr.responseText).toDoListData)
+        }
+
+    }
+
+    xhr.send()
+}
+
+showList()
